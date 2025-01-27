2297b36887488338c1353c7c8ae84b4b65c28552
pit9111 <dasilva.pit9111@gmail.com>
2025-01-27 20:42:46 +0100

    ajout des fichiers python + début de la refactorisation

---
+++ docs/model_training.py
@@ -0,0 +1,16 @@
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
+
